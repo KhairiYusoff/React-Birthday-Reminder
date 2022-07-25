03eb603f598b915bd742d14fe4cfba2664323d0b
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-07-25 15:22:16 +0800

    Birthday

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        id: 1,
+        name: 'Britney Heard',
+        age: 29,
+        image:
+            'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80',
+    },
+    {
+        id: 2,
+        name: 'Hester Hogan',
+        age: 32,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+        id: 3,
+        name: 'Larry Big',
+        age: 36,
+        image:
+            'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
+    },
+    {
+        id: 4,
+        name: 'Harry Genius',
+        age: 34,
+        image:
+            'https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+        id: 5,
+        name: 'Lola Gardner',
+        age: 29,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
+
+export default data;
